1e1ced4b59b3ccad546ff5eb5afba4721b181cec
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2016-03-24 20:11:52 -0700

    innnit

---
+++ replicat0r.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+
+#define TIMESTAMPSIZE 11 // 10 + room for a null
+
+int main(int argc, char **argv)
+{
+    char timestamp[TIMESTAMPSIZE];
+    snprintf(timestamp, TIMESTAMPSIZE, "%ld", time(NULL)); 
+
+    char *filename = argv[0];
+    char outfilename[TIMESTAMPSIZE];
+
+    strncat(outfilename, timestamp, TIMESTAMPSIZE);
+    printf("I am %s\n", filename);
+    printf("Gonna write %s\n", outfilename);
+
+    int fin = open(filename, O_RDONLY);
+    int fout = open(outfilename, O_WRONLY|O_CREAT, 0755);
+    printf("FOUT is %d\n", fout);
+
+    ssize_t ret;
+    ssize_t wret;
+#define BUFSIZE 4096
+    int buffer[BUFSIZE];
+    while ( (ret = read(fin, buffer, BUFSIZE)) != 0 ) {
+        wret = write(fout, buffer, BUFSIZE);
+        if ( wret == -1)
+            perror("bleurgh");
+    }
+
+    sleep(3);
+
+
+}
